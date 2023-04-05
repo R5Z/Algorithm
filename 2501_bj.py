af15eaf7bc7ba7b8fe1c994c8eb2a10444e461b5
R5Z Yoon <asaucybloom@gmail.com>
2023-04-05 15:43:36 +0900

    230405-2

--- 2501_bj.py
+++ 2501_bj.py
@@ -1,12 +1,25 @@
 N, K = map(int, input().split())
-divisor = []
+result = 0
 
 for i in range(1, N+1):
     if N%i == 0:
-        divisor.append(i)
+        K -= 1
+        if K == 0:
+            result = i
+            break
 
-if len(divisor) < K:
-    print(0)
-else:
-    print(divisor[K-1])
+print(result)
+
+
+# N, K = map(int, input().split())
+# divisor = []
+
+# for i in range(1, N+1):
+#     if N%i == 0:
+#         divisor.append(i)
+
+# if len(divisor) < K:
+#     print(0)
+# else:
+#     print(divisor[K-1])
 
