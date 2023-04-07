25be7d4cf9400cbf11b489f8f9043d7752c3feed
R5Z Yoon <asaucybloom@gmail.com>
2023-04-07 15:34:43 +0900

    230407

---
+++ 1979_bj.py
@@ -0,0 +1,14 @@
+N = int(input())
+numbers = list(map(int, input().split()))
+count = 0
+
+for x in numbers:
+    for i in range(2, x+1):
+        if x%i == 0:
+            if x == i:
+                count += 1
+            
+            break
+
+print(count)
+
