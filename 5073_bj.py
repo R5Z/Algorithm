2a2dc7e3f57be8db75a2593246910b7f25c55049
R5Z Yoon <asaucybloom@gmail.com>
2023-04-20 17:57:07 +0900

    230420

---
+++ 5073_bj.py
@@ -0,0 +1,15 @@
+while True:
+    a, b, c = map(int, input().split())
+    if a == b == c == 0: # a+b+c==0
+        break
+
+    if a == b == c:
+        print("Equilateral")
+    elif max(a, b, c) * 2 >= a + b + c:
+        print("Invalid")
+    elif a == b or b == c or c == a:
+        print("Isosceles")
+    else:
+        print("Scalene")
+
+    
