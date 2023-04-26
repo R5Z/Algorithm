9a894166e66a0a8258dc093e6eecfc8fa8462672
R5Z Yoon <asaucybloom@gmail.com>
2023-04-26 17:55:19 +0900

    230426-2

--- 24313_bj.py
+++ 24313_bj.py
@@ -3,3 +3,15 @@ c = int(input())
 n = int(input())
 x = 1 if a1*n+a0 <= c*n and c >= a1 else 0
 print(x)
+
+
+# same code
+
+# if score >= 60:
+#     message = "success"
+# else:
+#     message = "failure"
+
+# =
+
+# message = "success" if score >= 60 else "failure"
