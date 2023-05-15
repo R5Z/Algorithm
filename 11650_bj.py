50f55ac064b590ac13f55af152ddff99653c801a
R5Z Yoon <asaucybloom@gmail.com>
2023-05-15 14:11:42 +0900

    230515

---
+++ 11650_bj.py
@@ -0,0 +1,10 @@
+N = int(input())
+array = []
+
+for i in range(N):
+    a, b = map(int, input().split())
+    array.append((a, b))
+array.sort()
+for i in range(N):
+    print(array[i][0], array[i][1])
+
