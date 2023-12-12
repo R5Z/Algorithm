40df5cd0389225a60141372eafe126dfa39c5d13
R5Z Yoon <asaucybloom@gmail.com>
2023-12-12 18:25:06 +0900

    231212

--- sql/string-date/find-a-name_lv2.sql
+++ sql/string-date/find-a-name_lv2.sql
@@ -3,4 +3,4 @@
 
 ELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
 FROM ANIMAL_INS
-WHERE NAME IN('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+WHERE NAME IN('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty');
