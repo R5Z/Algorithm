3a6a1d47a3e5e0b2ca35c7e463f0c24775c8514e
R5Z Yoon <asaucybloom@gmail.com>
2023-12-20 17:21:17 +0900

    231220

---
+++ sql/select/sort-conditions-6_lv1.sql
@@ -0,0 +1,6 @@
+/* 어린 동물 찾기
+    programmers */
+
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE INTAKE_CONDITION != 'Aged';
---
+++ sql/select/sort-output-6_lv1.sql
@@ -0,0 +1,5 @@
+/* 동물의 아이디와 이름
+    programmers */
+
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS;
